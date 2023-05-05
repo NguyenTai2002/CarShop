fa944fe8257ca6a7893df2b7a353e68229075ad7
NguyenTai2002 <trinhnguyentai0610@gmail.com>
2023-05-05 22:51:17 +0700

    final commit

--- JS/tempCodeRunnerFile.js
+++ JS/tempCodeRunnerFile.js
@@ -1,11 +1,28 @@
-$(document).ready(function () {
-      let productId = localStorage.getItem('productId');
-      if (productId) {
-            let product = products.find(function (p) {
-                  return p.id == productId;
-            });
-            if (product) {
-                  displayProductDetail(product);
+      function handleLogin() {
+            if ($("#username").val() == "" || $("#password").val() == "") {
+                  $("input[id='username']").addClass("invalid")
+                  $("input[id='password']").addClass("invalid")
+            }else{
+                  return false;
             }
+            if (!regexUsernameOrEmail.test($("#username").val())) {
+                  $("input[id='username'] ~ span").html("Wrong username or email")
+                  $("input[id='username']").addClass("invalid")
+                  console.log("checked username")
+                  if (!regexPassword.test($("#password").val())) {
+                        $("input[id='password'] ~ span").html("Wrong username or email")
+                        $("input[id='password']").addClass("invalid")
+                        console.log("checked password")
+                  }
+                  return true;
+
+            } else {
+                  $("input[id='username'] ~ span").html("Accepted !")
+                  $("input[id='username']").addClass("success")
+                  $("input[id='password'] ~ span").html("Accepted !")
+                  $("input[id='password']").addClass("success")
+                  console.log("User accepted")
+                  return true;
+            }
+
       }
-});
